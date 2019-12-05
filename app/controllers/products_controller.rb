12e12f9aada8ac9628ef7089625b742f63933e6a
lumpirichard <richard.lumpi@sbg.at>
2019-12-05 21:41:43 +0000

    added functionality to add a product in the view to the cart, added 2 debug gems to gemfile

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,20 @@
 class ProductsController < ApplicationController
+
+  @@cart = []
+
   def catalogue
     @catalogue = Product.all
   end
 
   def view
+    @product = Product.find(params[:id].to_i)
   end
+
+  def add
+    @product = Product.find(params[:id].to_i)
+    @@cart << @product
+    @cart = @@cart
+  end
+
+
 end
