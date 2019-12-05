a45741faa5946d8e15c0675a29c0465bb3a4ff78
lumpirichard <richard.lumpi@sbg.at>
2019-12-05 20:41:58 +0000

    listing all products on main page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def catalogue
+    @catalogue = Product.all
   end
 
   def view
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'products#catalogue'
+
   get 'products/catalogue'
   get 'products/view'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
