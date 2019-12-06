608bbcb806303e44c2bb632b673be520ce65d7b9
lumpirichard <richard.lumpi@sbg.at>
2019-12-06 11:08:15 +0000

    added a max kalorie filter

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#catalogue'
 
+  get 'catalogue', to: 'products#catalogue'
   get 'products/catalogue'
   get 'products/cart', to: 'products#cart', as: :cart
   get 'products/:id', to: 'products#view', as: :product
