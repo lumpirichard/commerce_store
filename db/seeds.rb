3626ba3152f1f81e372ef462357eb8695c5e119a
lumpirichard <richard.lumpi@sbg.at>
2019-12-06 10:41:17 +0000

    fixed categories in seed to not exceed max characters of mysql field

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,15 @@ url = "https://raw.githubusercontent.com/salt-london/tech-challenge/master/data/
 csv_text = open(url)
 
 CSV.foreach(csv_text, headers: true,  encoding: "bom|utf-8", col_sep: "\t" ) do |row|
+  if row["categories"].nil?
+    cat = nil
+  else
+    cat = row["categories"][0..250]
+  end
+
   Product.create({
      name: row["product_name"],
-     # categories: row["categories"][0..50], #error too long for certain things
+     categories: cat, #error too long for certain things --> max for mysql is 255
      energy_kcal_100g: row["energy-kcal_100g"],
      url: row["url"]
   })
