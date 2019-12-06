a599cd2176d55e01d5520776e1dc85f1abbf6108
lumpirichard <richard.lumpi@sbg.at>
2019-12-06 11:08:15 +0000

    added a max kalorie filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,13 @@ class ProductsController < ApplicationController
   @@cart = []
 
   def catalogue
-    @catalogue = Product.all
+    if params[:calorie_count].nil?
+      @catalogue = Product.all
+    else
+      @catalogue = Product.select do |product|
+        product.energy_kcal_100g <= params[:calorie_count].to_f
+      end
+    end
   end
 
   def view
